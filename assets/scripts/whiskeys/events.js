ec86fedfb0632e55bb23b2c50782ccb56354e935
chardbres <richard.m.bresnahan@gmail.com>
2019-10-21 12:58:14 -0400

    Adjusted whiskey creation script.

--- assets/scripts/whiskeys/events.js
+++ assets/scripts/whiskeys/events.js
@@ -4,20 +4,27 @@ const getFormFields = require('../../../lib/get-form-fields.js')
 const api = require('./api.js')
 const ui = require('./ui.js')
 
-const onCreate = (event) => {
+const makeWhiskey = event => {
+  const name = $('.name').value()
+  const variety = $('.variety').value()
+  const region = $('.region').value()
+  const age = $('.age').value()
+  const taste = $('.taste').value()
+  const price = $('.price').price()
+
+  onCreate(name, variety, region, age, taste, price)
+}
+
+const onCreate = function (name, variety, region, age, taste, price) {
   event.preventDefault()
 
-  const name = $('.name').val()
-  const variety = $('.variety').val()
-  const region = $('.region').val()
-  const age = $('.age').val()
-  const taste = $('.taste').val()
-  const price = $('.price').val()
+  console.log(name, variety, region, age, taste, price)
   api.createWhiskey(name, variety, region, age, taste, price)
     .then(ui.onCreateWhiskeySuccess)
     .catch(ui.onCreateWhiskeyFailure)
 }
 
 module.exports = {
-  onCreate
+  onCreate,
+  makeWhiskey
 }
